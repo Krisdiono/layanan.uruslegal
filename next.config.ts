97948154ef79457eed2237758eabfd9babcc9f6f
Krisdiono <krisdiono@gmail.com>
2025-08-10 13:20:56 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
+  // sementara biar gampang build; nanti kita aktifkan lagi pelan-pelan
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+  // HAPUS kalau sebelumnya ada: output: "standalone"
 };
 
 export default nextConfig;
