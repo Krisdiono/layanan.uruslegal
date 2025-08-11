c2aea4cb019b7f61c28b4e3fd4372cc437c23cc7
Krisdiono <krisdiono@gmail.com>
2025-08-11 11:38:13 +0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -54,7 +54,8 @@ export default async function Page() {
   Detail
 </Link>
 <Link
-  href={`/layanan/${svc.slug}#bayar`}
+  <div id="bayar" />
+<OrderSummary svc={svc} /> href={`/layanan/${svc.slug}#bayar`}
   className="rounded-lg bg-emerald-600 px-3 py-1.5 text-sm text-white hover:bg-emerald-700"
 >
   Ajukan Proses
