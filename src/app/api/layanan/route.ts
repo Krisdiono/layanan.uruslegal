2145ef9cb5048b62fb1cfe997a76d7046659613e
Krisdiono <krisdiono@gmail.com>
2025-08-11 12:41:31 +0700

    Update route.ts

--- src/app/api/layanan/route.ts
+++ src/app/api/layanan/route.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from "next/server";
-const BASE = process.env.SOLUSI_API_BASE!; // ex: https://portal.uruslegal.id/ul/api
+const BASE = process.env.SOLUSI_API_BASE!; // https://portal.uruslegal.id/ul/api
 export const revalidate = 300;
-
 export async function GET() {
   const r = await fetch(`${BASE}/layanan`, { cache: "force-cache" });
   const j = await r.json();
